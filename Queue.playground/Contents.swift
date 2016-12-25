003b584dc8b410e2d0a0989b770b38daf9ddbdd3
Ronald Hernandez <2ronald.hernandez@gmail.com>
2016-12-25 15:50:48 -0500

    added queue

---
+++ Queue.playground/Contents.swift
@@ -0,0 +1,32 @@
+// implementation of a queue
+
+public struct Queue<T> {
+
+    fileprivate var array = [T]()
+
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+
+    public var count: Int {
+        return array.count
+    }
+
+    public mutating func enqueue(_ element: T) {
+        array.append(element)
+    }
+
+    public mutating func dequeue() -> T? {
+
+        if array.isEmpty {
+            return nil
+        } else {
+            return array.removeFirst()
+        }
+    }
+
+    public var front: T? {
+        return array.first
+    }
+
+}
