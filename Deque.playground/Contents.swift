d1fa284c31fe07c4f98deb8cf21771874ed8b4d6
Ronald Hernandez <2ronald.hernandez@gmail.com>
2016-12-25 17:46:22 -0500

    Implemented a Deque - a double ended enqueue

---
+++ Deque.playground/Contents.swift
@@ -0,0 +1,48 @@
+// Implementing a Deque
+
+
+public struct Deque<T> {
+
+    public var array = [T]()
+
+    public var isEmpty: Bool {
+        return count == 0
+    }
+
+    public var count: Int {
+        return array.count
+    }
+
+    public mutating func enqueue(_ element: T) {
+        array.append(element)
+    }
+
+    public mutating func enqueueFront(_ element: T) {
+        array.insert(element, at: 0)
+    }
+
+    public mutating func dequeue() -> T? {
+        if array.isEmpty {
+            return nil
+        } else {
+            return array.removeFirst()
+        }
+    }
+
+    public mutating func dequeueBack() -> T? {
+        if array.isEmpty {
+            return nil
+        } else {
+            return array.removeLast()
+        }
+    }
+
+    public var peekFront: T? {
+        return array.first
+    }
+
+    public var peekLast: T? {
+        return array.last
+    }
+    
+}
