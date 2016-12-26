2f4ded03669f0bdcddc6ccffb5f9af3723ce833d
Ronald Hernandez <2ronald.hernandez@gmail.com>
2016-12-26 08:32:56 -0500

    implemented linked list

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -29,4 +29,51 @@ public class LinkList<T> {
     public var first: Node? {
         return head
     }
+
+    public var last: Node? {
+        if var node = head {
+            while case let  next? = node.next {
+                node = next
+            }
+            return node
+        } else {
+            return nil
+        }
+    }
+
+    public func append(_ value: T) {
+        let newNode = Node(value: value)
+        if let lastNode = last {
+            newNode.previous = lastNode
+            lastNode.next = newNode
+        } else {
+            head = newNode
+        }
+    }
+
+    public var count: Int {
+        if var node = head {
+            var c = 1
+            while case let next? = node.next {
+                node = next
+                c += 1
+            }
+            return c
+        } else {
+            return 0
+        }
+    }
+
+    public func nodeAt(_ index: Int) -> Node? {
+        if index >= 0 {
+            var node = head
+            var i = index
+            while node != nil {
+                if i == 0 { return node }
+                i -= 1
+                node = node?.next
+            }
+        }
+        return nil
+    }
 }
