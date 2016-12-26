48ef63866a50db362dc3c532da8effb6edf03a8c
Ronald Hernandez <2ronald.hernandez@gmail.com>
2016-12-26 09:25:46 -0500

    added tree node

---
+++ Tree.playground/Contents.swift
@@ -0,0 +1,34 @@
+// Simple implementation of Tree 
+
+public class TreeNode<T> {
+
+    public var value: T?
+
+    public weak var parent: TreeNode?
+
+    public var children = [TreeNode<T>]()
+
+    public init(value: T) {
+        self.value = value
+    }
+
+    public func addChild(_ node: TreeNode<T>) {
+        children.append(node)
+        node.parent = self
+    }
+}
+
+
+
+
+
+
+extension TreeNode: CustomStringConvertible {
+    public var description: String {
+        var s = "\(value)"
+        if !children.isEmpty {
+            s += " {" + children.map { $0.description }.joined(separator: ", ") + "}"
+        }
+        return s
+    }
+}
