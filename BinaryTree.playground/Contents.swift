49a8e33ad5655a98322c0614f2a57c9646a41956
Ronald Hernandez <2ronald.hernandez@gmail.com>
2016-12-26 13:29:51 -0500

    updated BinaryTree implementation

---
+++ BinaryTree.playground/Contents.swift
@@ -0,0 +1,52 @@
+//: Implementation of a Binary Tree 
+
+
+public indirect enum BinaryTree<T> {
+    case node (BinaryTree<T>, T, BinaryTree<T>)
+    case empty
+
+}
+
+// leaf nodes
+let node5 = BinaryTree.node(.empty, "5", .empty)
+let nodeA = BinaryTree.node(.empty, "a", .empty)
+let node10 = BinaryTree.node(.empty, "10", .empty)
+let node4 = BinaryTree.node(.empty, "4", .empty)
+let node3 = BinaryTree.node(.empty, "3", .empty)
+let nodeB = BinaryTree.node(.empty, "b", .empty)
+
+// intermediate nodes on the left
+let Aminus10 = BinaryTree.node(nodeA, "-", node10)
+let timesLeft = BinaryTree.node(node5, "*", Aminus10)
+
+// intermediate nodes on the right
+let minus4 = BinaryTree.node(.empty, "-", node4)
+let divide3andB = BinaryTree.node(node3, "/", nodeB)
+let timesRight = BinaryTree.node(minus4, "*", divide3andB)
+
+// root node
+let tree = BinaryTree.node(timesLeft, "+", timesRight)
+
+extension BinaryTree: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case let .node(left, value, right):
+            return "value: \(value), left = [" + left.description + "], right = ["
+                + right.description + "]"
+        case .empty:
+            return ""
+        }
+    }
+}
+
+extension BinaryTree {
+    public var count: Int  {
+        switch self {
+        case let .node(left, _, right):
+            return left.count + 1 + right.count
+        case .empty:
+            return 0
+        }
+    }
+}
+
