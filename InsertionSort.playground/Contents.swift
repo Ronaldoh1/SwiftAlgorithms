ac1dae8fa1705a502eb7ed15881326e7c8add954
Ronald Hernandez <2ronald.hernandez@gmail.com>
2016-12-28 15:57:32 -0500

    updated Insertion sort to sort in place

--- InsertionSort.playground/Contents.swift
+++ InsertionSort.playground/Contents.swift
@@ -1,4 +1,24 @@
+// Implement Insertion Sort - in place
+
 // a simple array of unsorted integers
 
 let numberList: Array<Int> = [8, 2, 10, 9, 11, 1, 7, 3, 4]
 
+
+func insertionSort(_ array: [Int]) -> [Int] {
+
+    var numbers = array
+
+    for i in 1..<numbers.count {
+        var j = i
+        while j > 0 && numbers[j] < numbers[j - 1] {
+            swap(&numbers[j - 1], &numbers[j])
+            j -= 1
+        }
+    }
+
+    return numbers
+}
+
+
+insertionSort(numberList)
